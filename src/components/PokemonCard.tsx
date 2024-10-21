d43428579574cde2c84f8ea820b414845164d9fd
michaelgirardet <michaelgirardet@outlook.com>
2024-10-21 18:48:42 +0200

    change bulbizarre to bulbasaur

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,9 @@
+function Bulbasaure() {
+	return <><figure>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbizarre" />
+    </figure>
+    <figcaption>Bulbasaure</figcaption></>
+}
+
+export default Bulbasaure;
+
